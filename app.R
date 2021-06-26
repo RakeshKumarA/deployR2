962ab6b2c10c16af679a040a1e4287578c57043d
RakeshKumarA <rkakumar85@gmail.com>
2021-06-26 09:02:34 +0530

    new

--- app.R
+++ app.R
@@ -1,4 +1,10 @@
 library(plumber)
+my_packages <- c("quantmod")
+ install_if_missing <- function(p) {
+ if(p %in% rownames(installed.packages())==FALSE){
+ install.packages(p)}
+ }
+invisible(sapply(my_packages, install_if_missing))
 library(quantmod)
 
 port <- Sys.getenv('PORT')

b5da19b0edb61f05e170240a8e2bca9b373aaf88
RakeshKumarA <rkakumar85@gmail.com>
2021-06-26 19:52:54 +0530

    new

--- plumber.R
+++ plumber.R
@@ -1,5 +1,14 @@
 # plumber.R
 
+#* @preempt __first__
+#* @get /
+function(req, res) {
+  res$status <- 302
+  res$setHeader("Location", "./__docs__/")
+  res$body <- "Redirecting..."
+  res
+}
+
 
 #* Return chart
 #* @param add ticker in caps and add .NS for eg: TCS.NS, 
